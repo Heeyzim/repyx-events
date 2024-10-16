beffe9284419c2f69a95481df3372b9f7ec47911
Luigi Zampieri <zampieri.luigi17@gmail.com>
2024-10-15 23:52:49 -0300

    fix shared events

--- src/shared/type.ts
+++ src/shared/type.ts
@@ -1,12 +1,12 @@
 export const SharedEventType = {
   // Shared Event (on server)
-  SERVER_EVENT_HANDLER: `repix:events:shared:server:eventHandler`,
-  SERVER_CALLBACK_RECEIVER: `repix:events:shared:server:sendCallbackValues`,
+  SERVER_EVENT_HANDLER: `repyx:events:shared:server:eventHandler`,
+  SERVER_CALLBACK_RECEIVER: `repyx:events:shared:server:sendCallbackValues`,
 
   // Shared Event (on client)
-  CLIENT_EVENT_HANDLER: `repix:events:shared:client:eventHandler`,
-  CLIENT_CALLBACK_RECEIVER: `repix:events:shared:client:sendCallbackValues`,
+  CLIENT_EVENT_HANDLER: `repyx:events:shared:client:eventHandler`,
+  CLIENT_CALLBACK_RECEIVER: `repyx:events:shared:client:sendCallbackValues`,
 
   // Local Event
-  EVENT_HANDLER: `repix:events:local:eventHandler`,
+  EVENT_HANDLER: `repyx:events:local:eventHandler`,
 } as const
